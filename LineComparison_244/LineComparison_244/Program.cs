aa121e32d4c895521f5132417ee0f1476eae65a6
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-02 20:21:36 +0530

    Line comparison operation

---
+++ LineComparison_244/LineComparison_244/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lines_Pratice_Problem
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Program");
+        }
+    }
+}
