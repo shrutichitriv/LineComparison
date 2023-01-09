b50899f0221a4fd96fe236a1e1dc30416be487b2
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-09 19:19:51 +0530

    [Shruti] Solve using equals and compareTo method

---
+++ LineComparison_244/LineComparison_244/L4_LineLength.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison_244
+{
+    internal class L4_LineLength
+    {
+        public double GetLineLength(double x1, double y1, double x2, double y2)
+        {
+            double length = Math.Sqrt((Math.Pow((x2 - x1), 2)) + (Math.Pow((y2 - y1), 2)));
+            return length;
+        }
+    }
+}
--- LineComparison_244/LineComparison_244/Program.cs
+++ LineComparison_244/LineComparison_244/Program.cs
@@ -14,8 +14,57 @@ namespace LineComparison_244
              //L2_Equality2Lines.Equal();
              //Console.ReadLine();
 
-            L3_EqualGreaterLess.Greater();
-            Console.ReadLine();
+            //L3_EqualGreaterLess.Greater();
+            //Console.ReadLine();
+
+            //L4_LineLength
+            //First Line Point
+            Console.WriteLine("Enter the value of X1");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of Y1");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of X2");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of Y2");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            //Second Line Point
+            Console.WriteLine("Enter the value of X3");
+            double x3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of Y3");
+            double y3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of X4");
+            double x4 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of Y4");
+            double y4 = Convert.ToDouble(Console.ReadLine());
+
+
+
+            L4_LineLength linelength = new L4_LineLength();
+            double firstLineLength = linelength.GetLineLength(x1, y1, x2, y2);
+            double secondLineLength = linelength.GetLineLength(x3, y3, x4, y4);
+            Console.WriteLine($"{firstLineLength}    {secondLineLength}");
+
+            if (firstLineLength.Equals(secondLineLength))
+            {
+                Console.WriteLine("The line length are equal");
+            }
+            else if (firstLineLength.CompareTo(secondLineLength) > 0)
+            {
+                Console.WriteLine("First line is greater than Second line");
+            }
+            else
+            {
+                Console.WriteLine("Second line is greater than First Line");
+            }
+
         }
+    
     }
 }
